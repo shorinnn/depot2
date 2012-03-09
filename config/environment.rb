0ec3d7a763b98c6c2a4862d007a37536f97a89a1
shorinnn <shorinnn@yahoo.com>
2012-03-09 17:12:27 +0200

    Mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot2::Application.initialize!
+
+Depot2::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.delivery_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "domain.of.sender.net",
+    :authentication       => "plain",
+    :user_name            => "shorinnn",
+    :password             => "element0",
+    :enable_starttls_auto => true
+  } 
+end
